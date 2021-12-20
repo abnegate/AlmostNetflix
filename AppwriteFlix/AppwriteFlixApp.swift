b87ddb0ff7fa3f5761166bc865eaed7087f36b2b
Damodar Lohani <dlohani48@gmail.com>
2021-12-20 17:19:50 +0545

    Initial Commit

---
+++ AppwriteFlix/AppwriteFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppwriteFlixApp.swift
+//  AppwriteFlix
+//
+//  Created by Damodar Lohani on 20/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppwriteFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
